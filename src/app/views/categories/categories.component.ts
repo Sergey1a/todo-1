900353cbae32a62416be25e1e22a895ecc7f9962
Балятинских Сергей Александрович <sbaliatinskikh@sitronics.com>
2024-10-06 21:53:04 +0500

    Добавлено динамическое отображение категории в левом меню

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+  categories:Category[];
 
-  constructor() { }
+  constructor(private dataHandler: DataHandlerService) { }
 
   ngOnInit() {
+    this.categories = this.dataHandler.getCategories();
+    console.log(this.categories);
   }
-
 }
