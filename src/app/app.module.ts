244d66704d05686d3112200aca769cc97e55da80
Балятинских Сергей Александрович <sbaliatinskikh@sitronics.com>
2024-10-08 08:34:10 +0500

    Перенос списка задач на angular-material таблицу

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CategoriesComponent } from './views/categories/categories.component';
-import { TaskComponent } from './views/task/task.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
+
+import {CategoriesComponent} from './views/categories/categories.component';
+import {TaskComponent} from './views/task/task.component';
+
 
 @NgModule({
     declarations: [
@@ -14,7 +18,10 @@ import { TaskComponent } from './views/task/task.component';
     ],
     imports: [
         BrowserModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
